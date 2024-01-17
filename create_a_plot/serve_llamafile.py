33905cb764b54676c6f78bbb326f724239725227
asmith26 <asmith26@users.noreply.github.com>
2024-01-17 05:05:44 +0000

    Initial serve_llamafile.py

--- create_a_plot/serve_llamafile.py
+++ create_a_plot/serve_llamafile.py
@@ -1,65 +1,45 @@
-# Adapted from https://github.com/modal-labs/modal-examples/blob/main/10_integrations/panel/serve_panel.py
+# Adapted from https://github.com/modal-labs/modal-examples/blob/main/10_integrations/streamlit/serve_streamlit.py
 
 import pathlib
 
 import modal
 
-#  Container Dependencies
-#
-# Installs dependencies in panel_app.py and `asgiproxy` to proxy the Panel server.
-
+# Container Dependencies
+# installs dependencies in app.py and `asgiproxy` to proxy the llamafile server.
 image = (
     modal.Image.debian_slim()
     .apt_install("git")
-    .pip_install("panel", "numpy", "pandas")
     # Use fork until https://github.com/valohai/asgiproxy/pull/11 is merged.
     .pip_install("git+https://github.com/modal-labs/asgiproxy.git")
 )
+stub = modal.Stub(name="create_a_plot-llamafile", image=image)
 
-stub = modal.Stub(name="wikiplot", image=image)
+# Mount the llamafile script inside the container at a pre-defined path using a Modal mount
+llamafile_local_path = pathlib.Path(__file__).parent / "mistral-7b-instruct-v0.2.Q5_K_M.llamafile"
+llamafile_remote_path = pathlib.Path("/root/mistral-7b-instruct-v0.2.Q5_K_M.llamafile")
+llamafile_mount = modal.Mount.from_local_file(llamafile_local_path, llamafile_remote_path)
 
-# Mount the `app.py` script inside the container at a pre-defined path using a Modal mount
-panel_script_local_path = pathlib.Path(__file__).parent / "app.py"
-panel_script_remote_path = pathlib.Path("/root/app.py")
-
-if not panel_script_local_path.exists():
-    raise RuntimeError(
-        "app.py not found! Place the script with your panel app in the same directory."
-    )
-
-panel_script_mount = modal.Mount.from_local_file(
-    panel_script_local_path,
-    panel_script_remote_path,
-)
-
-# Spawning the Panel server
-# Inside the container, we will run the Panel server in a background subprocess using
+# Spawning the llamafile
+# Inside the container, we will run the llamafile in a background subprocess using
 # `subprocess.Popen`. Here we define `spawn_server()` to do this and then poll until the server
 # is ready to accept connections.
-HOST = "127.0.0.1"
-PORT = "8000"
+HOST = "0.0.0.0"
+PORT = "8080"
 
 
 def spawn_server():
     import socket
     import subprocess
-
     process = subprocess.Popen(
         [
-            "panel",
-            "run",
-            str(panel_script_remote_path),
-            "--browser.serverAddress",
+            "bash",
+            "./mistral-7b-instruct-v0.2.Q5_K_M.llamafile",
+            "--host",
             HOST,
-            "--server.port",
+            "--port",
             PORT,
-            "--browser.serverPort",
-            PORT,
-            "--server.enableCORS",
-            "false",
         ]
     )
-
     # Poll until webserver accepts connections before running inputs.
     while True:
         try:
@@ -76,19 +56,16 @@ def spawn_server():
                 )
 
 
-# ## Wrap it in an ASGI app
-#
+## Wrap it in an ASGI app
 # Finally, Modal can only serve apps that speak the [ASGI](https://modal.com/docs/guide/webhooks#asgi) or
 # [WSGI](https://modal.com/docs/guide/webhooks#wsgi) protocols. Since the Panel server is neither,
 # we run a separate ASGI app that proxies requests to the Panel server using the `asgiproxy` package.
 # Note that at this point `asgiproxy` has a bug with websocket handling, so we are using a
 # [fork](https://github.com/modal-labs/asgiproxy) with the fix for this.
-
-
 @stub.function(
     # Allows 1 concurrent requests per container.
     allow_concurrent_inputs=1,  # was 100
-    mounts=[panel_script_mount],
+    mounts=[llamafile_mount],
 )
 @modal.asgi_app()
 def run():
@@ -111,19 +88,13 @@ def run():
 
 
 # ## Iterate and Deploy
-#
-# While you're iterating on your screamlit app, you can run it "ephemerally" with `modal serve`. This will
+# Run it "ephemerally" with `modal serve`. This will
 # run a local process that watches your files and updates the app if anything changes.
 #
-# ```shell
 # modal serve serve_panel.py
-# ```
-#
+
 # Once you're happy with your changes, you can deploy your application with
-#
-# ```shell
-# modal deploy serve_panel.py
-# ```
-#
 # If successful, this will print a URL for your app, that you can navigate to from
 # your browser 🎉 .
+#
+# modal deploy serve_panel.py
